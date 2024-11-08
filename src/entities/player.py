74986ec7e9e16131c474d780791c40284cc02939
Никита <nikita_lysenko_04@mail.ru>
2024-11-08 14:21:06 +0700

    move

--- src/entities/player.py
+++ src/entities/player.py
@@ -26,11 +26,11 @@ class Player(pygame.sprite.Sprite):
 
         # Переменные измерений
         self.PLAYER_SPEED = 2
-        self.PLAYER_JUMP_POWER = 0.7
-        self.PLAYER_JUMP_HEIGHT = 50
-        self.PLAYER_POSITION = None
+        self.PLAYER_JUMP_POWER = 0.5
+        self.PLAYER_JUMP_HEIGHT = 200
+        self.PLAYER_POSITION = 0
 
-        self.GRAVITY = 0.6
+        self.GRAVITY = 0.02
         
     def load_sprites(self):
         self.sprites = {
@@ -53,16 +53,17 @@ class Player(pygame.sprite.Sprite):
         # Устанавливаем начальный спрайт
         self.image = self.sprites['idle'][0]
         self.rect = self.image.get_rect()
-    
+
     def update(self):
-        if not self.on_ground and not self.is_jump:
+        if not self.on_ground:
             self.apply_gravity()
+
         self.handle_movement()
         self.animate()
-        
+
     def handle_movement(self):
         keys = pygame.key.get_pressed()
-        
+
         # Горизонтальное движение
         self.velocity_x = 0
 
@@ -72,30 +73,21 @@ class Player(pygame.sprite.Sprite):
         if keys[pygame.K_RIGHT]:
             self.velocity_x = self.PLAYER_SPEED
             self.facing_right = True
-            
+
         # Прыжок
-        if keys[pygame.K_SPACE] and self.on_ground:
-            self.velocity_y = self.PLAYER_JUMP_POWER
-            self.PLAYER_POSITION = 0
-            self.is_jump = True
+        if keys[pygame.K_UP] and self.on_ground:
+            self.velocity_y -= self.PLAYER_JUMP_POWER
             self.on_ground = False
 
-        if self.is_jump and self.PLAYER_JUMP_HEIGHT > self.PLAYER_POSITION:
-            self.velocity_y += self.PLAYER_JUMP_POWER
-            self.rect.y -= self.velocity_y
-            self.PLAYER_POSITION += self.velocity_y
-            print("***********", self.PLAYER_JUMP_HEIGHT, self.PLAYER_POSITION)
-        else:
-            self.is_jump = False
-            print(self.is_jump)
-
+        # Обновляем позицию игрока
         self.rect.x += self.velocity_x
-        self.rect.y -= self.velocity_y
+        self.rect.y += self.velocity_y
 
     def apply_gravity(self):
-        self.velocity_y += self.GRAVITY
-        self.rect.y += self.velocity_y
-        
+        if not self.on_ground:
+            self.velocity_y += self.GRAVITY  # Применяем гравитацию к вертикальной скорости
+
+
     def animate(self):
         if not self.on_ground:
             self.current_animation = 'jump' if self.velocity_y < 0 else 'fall'
